daa7dcdfa18be3e7b7949bc9fe12fbea1e245eda
m-s-puglielli <pugliell21@up.edu>
2020-02-15 17:01:33 -0800

    converted indentation to tabs

--- hw3/routes/index.js
+++ hw3/routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-let a = 0;
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  a = 1;
+router.get('/', function(req, res, next)
+{
+	res.sendFile("CheeseCake.html");
 });
-
-console.log(a);
 
 module.exports = router;
