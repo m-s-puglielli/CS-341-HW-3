26d666a072930b5f8ba3fbcc13ecc5405427546a
m-s-puglielli <pugliell21@up.edu>
2020-02-15 16:18:32 -0800

    Changes for HW #4

--- hw3/public/javascripts/sum.js
+++ hw3/public/javascripts/sum.js
@@ -1,3 +1,10 @@
+/*
+Maximilian Puglielli
+02/03/2020 @ 11:55pm
+CS-341-B Spring-2020
+Homework #3
+*/
+
 function sum(a, b)
 {
 	return a + b;
--- hw3/tests/sum.tests.js
+++ hw3/tests/sum.tests.js
@@ -1,3 +1,10 @@
+/*
+Maximilian Puglielli
+02/03/2020 @ 11:55pm
+CS-341-B Spring-2020
+Homework #3
+*/
+
 const sum = require("../public/javascripts/sum.js");
 test("adds 1 + 2 to equal 3", () =>
 {
