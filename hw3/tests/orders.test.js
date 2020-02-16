1b4b1fce2df8d0a24c8f250d3383e33da23d201f
m-s-puglielli <pugliell21@up.edu>
2020-02-15 19:26:38 -0800

    HW #4 is done!!!

---
+++ hw3/tests/orders.test.js
@@ -0,0 +1,51 @@
+/*
+Maximilian Puglielli
+02/03/2020 @ 11:55pm
+CS-341-B Spring-2020
+Homework #3
+*/
+const app = require("../app");
+const http = require("http");
+var serverVal;
+
+beforeAll(() =>
+{
+    serverVal = app.listen(3000);
+});
+
+const orders = require("../routes/orders.js");
+test("checks if orders.js posts a valid json object", done =>
+{
+    const options = {
+        hostname: "localhost",
+        port: "3000",
+        path: "/orders",
+        method: "POST"
+    };
+
+    var bodyChunks = [];
+    http.get(options, function(result)
+    {
+        result.on("data", function(chunk)
+        {
+            bodyChunks.push(chunk);
+        }).on("end", function()
+        {
+            let body = Buffer.concat(bodyChunks);
+            try
+            {
+                JSON.parse(body);
+                done();
+            }
+            catch (e)
+            {
+                done(e);
+            }
+        });
+    });
+});
+
+afterAll(() =>
+{
+    serverVal.close();
+});
