e0c3ed6ca3e800851f8a8b42a239c44de4f33be7
m-s-puglielli <pugliell21@up.edu>
2020-02-15 16:46:23 -0800

    Orders router implemented

--- hw3/routes/index.js
+++ hw3/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let a = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  a = 1;
 });
+
+console.log(a);
 
 module.exports = router;
